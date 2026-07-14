556d8c6526fbe40f69ab6634d2b4b2d023e42f7f
alireza <alireza808590100@gmail.com>
2026-07-14 14:55:35 +0430

    time zone configuration

--- backend/A/settings.py
+++ backend/A/settings.py
@@ -123,9 +123,9 @@ AUTH_USER_MODEL = 'accounts.User'
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'  # fa-ir
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
